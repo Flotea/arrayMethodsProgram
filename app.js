5a76351ce8c608fb952dce43a979a69b5e702b8a
flotea <hgrimmer0786@myhta.org>
2019-05-03 21:01:12 -1000

    finished Js

---
+++ app.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+    var text = document.getElementById("addOn");
+var submit = document.getElementById("submit");
+var fin = document.getElementById("fin");
+var list=[];
+var visList = document.getElementById("list");
+console.log(list);
+visList.innerHTML = list;
+
+submit.addEventListener("click", function(){
+    var add = text.value;
+    list.push(add);
+    console.log(list);
+    var listA = document.createElement("LI");
+    var addOn = document.createTextNode(text.value);
+    listA.appendChild(addOn);
+    visList.appendChild(listA);
+
+    //visList.innerHTML = list;
+
+});
+
+fin.addEventListener("click", function(){
+    list.shift();
+    console.log(list);
+    visList.removeChild(visList.childNodes[0]); 
+
+});
+
+
+}
